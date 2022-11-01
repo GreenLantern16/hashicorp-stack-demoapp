1a605e694ac913afa9e776c49984abef8c54f708
Greg Thomas <88564478+GreenLantern16@users.noreply.github.com>
2022-11-01 09:12:08 -0400

    Update vpc.tf

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -19,7 +19,7 @@ module "vpc" {
 
   name             = var.name
   cidr             = var.vpc_cidr_block
-  azs              = data.aws_availability_zones.available.names
+  azs              = ["us-east1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east2a", "us-east-2b", "us-east-2c", "us-east-2d", "us-east-2e"]
   private_subnets  = slice(local.subnets, 0, 3)
   public_subnets   = slice(local.subnets, 3, 6)
   database_subnets = slice(local.subnets, 6, 9)
